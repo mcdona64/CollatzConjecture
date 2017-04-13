1287b28dfe754d932d8fd28d7743f4363e607013
mcdona64 <mcdona64@purdue.edu>
2017-04-12 21:44:44 -0400

    Commit

--- collatz.cpp
+++ collatz.cpp
@@ -5,37 +5,43 @@
 #include <time.h>
 
 int main(int argc, char* argv[]) {
+	//make sure that there was the correct numbers entered
 	if (argc < 0) {
 		printf("ussage error type the range of numbers you want to check");
 		return 0;
 	}
-	bool faild = 0;
+	
+	//make nessicarly local variables 
 	int start = atoi(argv[1]);
 	double n = (double)start;
 	int end = atoi(argv[2]);
 	clock_t sclock, eclock;
+
+	//start the clock to time the program
 	sclock = clock();
-	while (faild == 0 && n <= end) {
+	while (n <= end) {
+
+		//run the function untill it is zero
 		double mut = n;
-		while (mut != 1 && !(start < mut && mut < n)) {
+		while (mut != 1 && !(start < mut && mut < n)) { // if the second condition is true then we know that it will work
 			if ((int)mut % 2 == 0){
 	 			mut = mut / 2;
 			} else if ((int)mut % 2 == 1){
 				mut = mut * 3 + 1;
 			}
-			//printf("%lf\n",mut);
 		}
 		n++;
+
 	}
+
+	//end the clock
 	eclock = clock();
-	if (faild != 0){
-		printf("it faild at %lf\n", n);
-	} else {
-		printf("it worked\n");
-		std::ofstream highscore;
-		highscore.open("highscores.txt", std::fstream::app);
-		highscore << "start: " << start << "\t|\tend: " << end << "\t|\t time: " << (double)((eclock - sclock)/CLOCKS_PER_SEC) << "\n";
-		highscore.close();
-	}
+
+	//print results to the screan and file
+	printf("it worked\n");
+	std::ofstream highscore;
+	highscore.open("highscores.txt", std::fstream::app);
+	highscore << "start: " << start << "\t|\tend: " << end << "\t|\t time: " << (double)((eclock - sclock)/CLOCKS_PER_SEC) << "\n";
+	highscore.close();
 	return 0;
 }
